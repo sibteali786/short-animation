4b62b3143979ca5adbbc43acd0668e517b55e98a
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-05 22:22:29 +0500

    Added many things

--- project4-A/main.js
+++ project4-A/main.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function () {
   var shapeElement = document.querySelector(".clouds");
+  var shapeElementCloud = document.querySelector(".clouds1");
+  var shapeElementCloud_big = document.querySelector(".clouds2");
+  var shapePole = document.querySelector(".pole");
+  var shapePole1 = document.querySelector(".pole_1");
+  var shapeMoon = document.querySelector(".Moon");
   var frames = [
     { transform: "translateX(100%)" },
     { transform: "translateX(-100%)" },
@@ -9,5 +14,26 @@ window.addEventListener("load", function () {
     duration: 6000,
     iterations: Infinity,
   };
+  var timingMoon = {
+    duration: 40000,
+    iterations: Infinity,
+  };
+  var timingCloud = {
+    duration: 9000,
+    iterations: Infinity,
+  };
+  var timingCloud_big = {
+    duration: 12000,
+    iterations: Infinity,
+  };
+  var timingPole = {
+    duration: 7000,
+    iterations: Infinity,
+  };
+  shapeElementCloud_big.animate(frames, timingCloud_big);
+  shapeElementCloud.animate(frames, timingCloud);
   shapeElement.animate(frames, timing);
+  shapePole.animate(frames, timingPole);
+  shapePole1.animate(frames, timingPole);
+  shapeMoon.animate(frames, timingMoon);
 });
